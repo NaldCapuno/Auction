3e3712fc18c3e3c2780b33e452f4e580be149270
NaldCapuno <jasoncapuno@gmail.com>
2023-12-16 14:48:24 +0800

    update

--- simple_echo_client (1).py
+++ simple_echo_client (1).py
@@ -10,41 +10,42 @@ class EchoClient(BanyanBase):
 
         self.set_subscriber_topic('reply')
 
-        self.client_name = ''
-
         def accept():
-            def client_window(name):
-                self.client = tk.Tk()
-                self.client.title(f"CLIENT {name}")
-                self.client.resizable(False, False)
-                
-                self.button_bid = tk.Button(text="Bid", width=10)
-                self.button_bid.grid(row=0, column=0, padx=10, pady=10)
-
-                self.button_sell = tk.Button(text="Sell", width=10)
-                self.button_sell.grid(row=0, column=1, padx=5, pady=10)
-
-                self.label_time = tk.Label(text=f"Time Left: second(s)")
-                self.label_time.grid(row=0, column=2, padx=5, pady=10)
-
-                self.client.mainloop()
-
-            self.name = self.entry.get()
-            self.client_name = self.name
+            self.client_name = self.main_entry.get()
             self.publish_payload(self.client_name, 'echo')
             self.main.destroy()
-            client_window(self.client_name)
+ 
+            # client window
+            self.client = tk.Tk()
+            self.client.title(f"CLIENT {self.client_name}")
+            self.client.resizable(False, False)
+            
+            self.client_button_bid = tk.Button(self.client, text="Bid", width=10)
+            self.client_button_bid.grid(row=0, column=0, padx=10, pady=10)
 
-        # window
+            self.client_button_sell = tk.Button(self.client, text="Sell", width=10)
+            self.client_button_sell.grid(row=0, column=1, padx=5, pady=10)
+
+            self.client_label_time = tk.Label(self.client, text=f"Time Left:")
+            self.client_label_time.grid(row=0, column=2, padx=5, pady=10)
+
+            self.client_text_time = tk.Text(self.client, width=5, height=1)
+            self.client_text_time.insert(tk.END, self.time)
+            self.client_text_time.configure(state=tk.DISABLED)
+            self.client_text_time.grid(row=0, column=3, padx=10, pady=10)
+
+            self.client.mainloop()
+
+        # main window
         self.main = tk.Tk()
         self.main.title("ENTER YOUR NAME")
         self.main.resizable(False, False)
 
-        self.entry = tk.Entry(self.main, width=35)
-        self.entry.grid(row=0, column=0, padx=10, pady=10)
+        self.main_entry = tk.Entry(self.main, width=35)
+        self.main_entry.grid(row=0, column=0, padx=10, pady=10)
 
-        self.button = tk.Button(self.main, text="Accept", command=accept)
-        self.button.grid(row=0, column=1, padx=10, pady=10)
+        self.main_button = tk.Button(self.main, text="Accept", command=accept)
+        self.main_button.grid(row=0, column=1, padx=10, pady=10)
 
         # Start the Banyan receive loop in a separate thread
         self.thread = threading.Thread(target=self.receive_loop)
@@ -52,8 +53,9 @@ class EchoClient(BanyanBase):
 
         self.main.mainloop()
 
+
     def incoming_message_processing(self, topic, payload):
-        print(payload)
+        self.time = payload
 
 
 def echo_client():
--- simple_echo_server (1).py
+++ simple_echo_server (1).py
@@ -10,13 +10,26 @@ class EchoServer(BanyanBase):
 
         self.set_subscriber_topic('echo')
 
+        def start():
+            self.time = int(self.entry.get())
+            self.publish_payload(self.time, 'reply')
+
         # window
         self.main = tk.Tk()
         self.main.title("SERVER")
         self.main.resizable(False, False)
 
-        self.textbox = tk.Text(self.main, width=35, height=20, state=tk.DISABLED)
-        self.textbox.grid(row=0, column=0, padx=10, pady=10)
+        self.textbox = tk.Text(self.main, width=45, height=20, state=tk.DISABLED)
+        self.textbox.grid(row=0, column=0, padx=10, pady=10, columnspan=3)
+
+        self.label = tk.Label(self.main, text="Countdown (Seconds):")
+        self.label.grid(row=1, column=0, padx=10, pady=10)
+
+        self.entry = tk.Entry(self.main)
+        self.entry.grid(row=1, column=1, padx=5, pady=10)
+
+        self.button = tk.Button(self.main, text="Start", command=start)
+        self.button.grid(row=1, column=2, padx=10, pady=10)
 
         # Start the Banyan receive loop in a separate thread
         self.thread = threading.Thread(target=self.receive_loop)
@@ -28,7 +41,6 @@ class EchoServer(BanyanBase):
         self.textbox.configure(state=tk.NORMAL)
         self.textbox.insert(tk.END, f"{payload} is ready...\n")
         self.textbox.configure(state=tk.DISABLED)
-        print(payload, 'is ready...')
 
 
 def echo_server():
